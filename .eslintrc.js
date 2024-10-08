1ba25201a15827a95719cb142721459913c2aff6
Michael Lim <mlim10@northwell.edu>
2024-10-08 11:07:47 -0500

    [build] update development tools - eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: 'expo',
+  env: {
+    node: true,
+  },
+};
--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -9,12 +9,12 @@ import { initializeApp } from 'firebase/app';
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBAWrVnjzYynscaIwsqVZXNPfbPJST9X48",
-  authDomain: "todofireapp-efbe5.firebaseapp.com",
-  projectId: "todofireapp-efbe5",
-  storageBucket: "todofireapp-efbe5.appspot.com",
-  messagingSenderId: "448581803953",
-  appId: "1:448581803953:web:ff162f514dd555ac842790"
+  apiKey: 'AIzaSyBAWrVnjzYynscaIwsqVZXNPfbPJST9X48',
+  authDomain: 'todofireapp-efbe5.firebaseapp.com',
+  projectId: 'todofireapp-efbe5',
+  storageBucket: 'todofireapp-efbe5.appspot.com',
+  messagingSenderId: '448581803953',
+  appId: '1:448581803953:web:ff162f514dd555ac842790',
 };
 
 const app = initializeApp(firebaseConfig);
