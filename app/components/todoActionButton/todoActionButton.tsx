7b55a64959ba1f3de6a1f02c7aa8add6200b7327
Michael Lim <111325098+michaeldslim@users.noreply.github.com>
2024-12-20 10:58:29 -0600

    [feat] Add GPLv3 copyright headers and configure ESLint rule (#27)
    Added GNU GPLv3 copyright headers to all source files to comply with licensing requirements.
    
    Co-authored-by: Michael Lim <mlim10@northwell.edu>

--- app/components/todoActionButton/todoActionButton.tsx
+++ app/components/todoActionButton/todoActionButton.tsx
@@ -1,3 +1,6 @@
+/*
+Copyright (C) 2024 Michael Lim - React Native Todo App - This software is free to use, modify, and share under the terms of the GNU General Public License v3.
+*/
 import React from 'react';
 import { TouchableOpacity, Text, StyleProp, ViewStyle } from 'react-native';
 
--- copyright/header.js
+++ copyright/header.js
@@ -1,13 +1,3 @@
 /*
 Copyright (C) 2024 Michael Lim - React Native Todo App - This software is free to use, modify, and share under the terms of the GNU General Public License v3.
 */
-module.exports = {
-  extends: 'expo',
-  plugins: ['header'],
-  rules: {
-    'header/header': [2, 'copyright/header.js'],
-  },
-  env: {
-    node: true,
-  },
-};
